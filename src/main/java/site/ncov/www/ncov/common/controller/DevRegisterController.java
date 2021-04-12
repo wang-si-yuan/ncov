c6d033d63435b2202780d02d7ba23aaede5e969b
wangsiyuan <2650306917@qq.com>
2021-04-12 23:44:57 +0800

    feat(身份证识别测试): 身份证识别测试

--- src/main/java/site/ncov/www/ncov/common/controller/DevRegisterController.java
+++ src/main/java/site/ncov/www/ncov/common/controller/DevRegisterController.java
@@ -1,6 +1,7 @@
 package site.ncov.www.ncov.common.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.ncov.www.ncov.common.model.dto.RegisterDto;
@@ -23,6 +24,7 @@ public class DevRegisterController {
     @Autowired
     private IDCardOCRService idCardOCRService;
 
+    @PostMapping
     public HttpResult setUser(RegisterDto registerDto) throws FileNotFoundException {
         User user = idCardOCRService.getUserByOCR(new Picture(registerDto.getIdCard()));
         if (user!=null) {
