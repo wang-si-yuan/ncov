454ab8f233c1f1d65087ae63c4678f541e1dac52
wangsiyuan <2650306917@qq.com>
2021-04-12 11:53:15 +0800

    feat(集成rabbit mq): 集成rabbit mq
    集成rabbit mq，添加依赖和配置文件

---
+++ src/main/java/site/ncov/www/ncov/common/config/RabbitConfig.java
@@ -0,0 +1,13 @@
+package site.ncov.www.ncov.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 王思源
+ * @version 0.0.0
+ */
+
+@Configuration
+public class RabbitConfig {
+
+}
