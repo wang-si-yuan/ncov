30bcf4cf1ae5984cd803fa4e7cf88782a81eef6b
wangsiyuan <2650306917@qq.com>
2021-05-28 15:36:57 +0800

    feat(获取7天内到访城市): 获取7天内到访城市

---
+++ src/main/java/site/ncov/www/ncov/common/config/WebConfig.java
@@ -0,0 +1,23 @@
+package site.ncov.www.ncov.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author 王思源
+ * @version 0.0.0
+ */
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE","OPTIONS")
+                .maxAge(3600);
+    }
+}
