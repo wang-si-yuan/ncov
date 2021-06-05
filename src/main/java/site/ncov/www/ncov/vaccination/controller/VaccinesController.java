db23982dd8df3486b9d88741c436e5474c281c3e
wangsiyuan <2650306917@qq.com>
2021-06-05 22:52:52 +0800

    feat(获取7天内到访城市): 获取7天内到访城市

--- src/main/java/site/ncov/www/ncov/vaccination/controller/VaccinesController.java
+++ src/main/java/site/ncov/www/ncov/vaccination/controller/VaccinesController.java
@@ -1,10 +1,13 @@
 package site.ncov.www.ncov.vaccination.controller;
 
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import site.ncov.www.ncov.common.domain.entity.HttpResult;
 import site.ncov.www.ncov.vaccination.controller.cqe.VaccinesCommand;
@@ -25,7 +28,8 @@ public class VaccinesController {
     @Autowired
     private VaccinesService vaccinesService;
 
-    @RequestMapping("/addVaccines")
+    @ApiOperation("添加疫苗")
+    @RequestMapping(value = "/addVaccines", method = {RequestMethod.POST})
     public HttpResult addVaccines(VaccinesCommand vaccinesCommand) {
         vaccinesService.addVaccines(vaccinesCommand.transEntity());
         return HttpResult.ok();
